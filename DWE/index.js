eb88fe089e802c51e6093efe338a60c490d104e2
Biel . <adriellebintang@gmail.com>
2023-11-20 14:32:46 +0700

    update

---
+++ DWE/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    console.log($('h1'))
+    $('label').css('text-align','center');
+    $('.input-control').css('display','flex')
+    $('.input-control').css('flex-direction','column')
+})
